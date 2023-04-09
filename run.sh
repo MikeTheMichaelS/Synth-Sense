2b55e17194ef60cf66d6c0f4678244179220217b
Yiyun Sun <yiyunsun@bu.edu>
2023-04-09 17:17:35 -0400

    Add startup script v0.1

---
+++ run.sh
@@ -0,0 +1,20 @@
+echo "================================="
+echo "==  SynthSense Startup Script  =="
+echo "==          V 0.1              =="
+echo "==                             =="
+echo "==  This window will remain    =="
+echo "==  open while other program   =="
+echo "==  is running.                =="
+echo "================================="
+
+# running
+echo "Installing npm dependencies"
+cd ./frontend && npm install
+echo "Starting backend process"
+cd .. && cd ./backend && uvicorn main:app --reload&
+echo "Backend started"
+echo "Starting frontend process"
+cd .. && cd ./frontend && npm start&
+echo "Frontend started"
+echo "Startup process success"
+echo "Open 'http://localhost:3000' to access the program"
