c8792e40186cc5e3bdd8f64299eb6f5dbb7ce52a
Yiyun Sun <yiyunsun@bu.edu>
2023-04-10 14:59:23 -0400

    Disable startup script

--- run.sh
+++ run.sh
@@ -8,13 +8,14 @@ echo "==  is running.                =="
 echo "================================="
 
 # running
-echo "Installing npm dependencies"
-cd ./frontend && npm install
-echo "Starting backend process"
-cd .. && cd ./backend && uvicorn main:app --reload&
-echo "Backend started"
-echo "Starting frontend process"
-cd .. && cd ./frontend && npm start&
-echo "Frontend started"
-echo "Startup process success"
-echo "Open 'http://localhost:3000' to access the program"
+#echo "Installing npm dependencies"
+#cd ./frontend && npm install
+#echo "Starting backend process"
+#cd .. && cd ./backend && uvicorn main:app --reload&
+#echo "Backend started"
+#echo "Starting frontend process"
+#cd .. && cd ./frontend && npm start&
+#echo "Frontend started"
+#echo "Startup process success"
+#echo "Open 'http://localhost:3000' to access the program"
+echo "Script disabled due to unstableness"
